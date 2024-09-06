773592da2d74f73cf631f4af911ebc2f7d94065b
ilia Sotnikov <ilia12st@yandex.ru>
2024-09-06 16:35:49 +0300

    do sequential multiplication

---
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication/Program.cs
@@ -0,0 +1,10 @@
+﻿using ParallelMatrixMultiplication;
+
+List<List<int>> m1 = [[1, 2], [3, 4]];
+List<List<int>> m2 = [[3, 4], [1, 2]];
+
+var m3 = SecquentialMatrixMultiplication.Multiply(m1, m2);
+foreach (var innerList in m3)
+{
+    Console.WriteLine(string.Join(", ", innerList));
+}
---
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication/SequentialMatrixMultiplication.cs
@@ -0,0 +1,32 @@
+﻿namespace ParallelMatrixMultiplication;
+
+public static class SecquentialMatrixMultiplication
+{
+    public static List<List<int>> Multiply(List<List<int>> matrix1, List<List<int>> matrix2)
+    {
+        if (matrix1.Count != matrix2[0].Count)
+        {
+            throw new ArgumentException("The number of rows of the first matrix must be equal to the number of columns of the other matrix.");
+        }
+
+        var result = new List<List<int>>();
+        for (int i = 0; i < matrix1.Count; ++i)
+        {
+            result.Add(new List<int>(new int[matrix2[0].Count]));
+        }
+
+        for (int i = 0; i < matrix1.Count; ++i)
+        {
+            for (int j = 0; j < matrix2[0].Count; ++j)
+            {
+                result[i][j] = 0;
+                for (int k = 0; k < matrix1[0].Count; ++k)
+                {
+                    result[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+}
