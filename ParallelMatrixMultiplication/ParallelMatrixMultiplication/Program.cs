46d2b4ffa3d9729b74462438a75a57b7c0d5cf3c
ilia Sotnikov <ilia12st@yandex.ru>
2024-09-11 23:40:48 +0300

    add file support

--- ParallelMatrixMultiplication/ParallelMatrixMultiplication/Program.cs
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication/Program.cs
@@ -1,47 +1,95 @@
 ﻿using ParallelMatrixMultiplication;
 
-static void RunBenchmark()
+
+public class Program
 {
-    for (int size = 100; size < 1001; size += 100)
+    static void RunBenchmark()
     {
-        MatrixMultiplicationBenchmark.Benchmark(size, 3);
+        for (int size = 100; size < 1001; size += 100)
+        {
+            MatrixMultiplicationBenchmark.Benchmark(size, 3);
+        }
+
+        MatrixMultiplicationBenchmark.MakeGraph();
     }
 
-    MatrixMultiplicationBenchmark.MakeGraph();
-}
-
-
-
-void Main(string[] args)
-{
-    if (args.Length == 0)
+    static int[][] ExtractMatrix(string filename)
     {
-        Console.WriteLine("There are no arguments.\nUse -help for help\n-m <filename> to multiply the matrices\n-b to run the benchmark.");
-        return;
+        if (!File.Exists(filename))
+        {
+            throw new FileNotFoundException(filename);
+        }
+
+        var lines = File.ReadAllLines(filename);
+        int[][] result = new int[lines.Length][];
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            result[i] = Array.ConvertAll(lines[i].Split(' '), int.Parse);
+        }
+
+        return result;
     }
 
-    switch (args[0])
+    static void EnterMatrix(int[][] matrix)
     {
-        case "-help":
-            Console.WriteLine("Use -m <filename> to multiply the matrices\n-b to run the benchmark.");
-            break;
-        case "-m":
-            if (args.Length < 3)
-            {
-                Console.WriteLine("Enter filename for multiplication.");
-                return;
-            }
-            string filename1 = args[1];
-            string filename2 = args[2];
-            break;
+        StreamWriter sw = new("result.txt");
+        for (int i = 0; i < matrix.Length; ++i)
+        {
+            sw.WriteLine(String.Join(" ", matrix[i]));
+        }
 
-        case "-b":
-            RunBenchmark();
-            break;
-
-        default:
-            Console.WriteLine("Unknown command.\nUse - m < filename > to multiply the matrices\n-b to run the benchmark.");
-            break;
+        sw.Close();
     }
 
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("There are no arguments.\nUse -help for help\n-m <filename> to multiply the matrices\n-b to run the benchmark.");
+            return;
+        }
+
+        switch (args[0])
+        {
+            case "-help":
+                Console.WriteLine("Use -m <filename> to multiply the matrices\n-b to run the benchmark.");
+                break;
+
+            case "-m":
+                if (args.Length < 3)
+                {
+                    Console.WriteLine("Enter filename for multiplication.");
+                    return;
+                }
+                string filename1 = args[1];
+                string filename2 = args[2];
+
+                try
+                {
+                    int[][] matrix1 = ExtractMatrix(filename1);
+                    int[][] matrix2 = ExtractMatrix(filename2);
+
+                    int[][] result = ParallelMultiplication.Multiply(matrix1, matrix2);
+
+                    EnterMatrix(result);
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.WriteLine("File not found.");
+                    return;
+                }
+
+                Console.WriteLine("OK");
+                break;
+
+            case "-b":
+                RunBenchmark();
+                break;
+
+            default:
+                Console.WriteLine("Unknown command.\nUse - m < filename > to multiply the matrices\n-b to run the benchmark.");
+                break;
+        }
+    }
 }
