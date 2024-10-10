0892975a09c69ed04240c19aa2c2b8d3479c7819
Ilya Sotnikov <ilia12st@yandex.ru>
2024-10-10 16:24:26 +0300

    add documentation

--- MyThreadPool/MyThreadPool/MyThreadPool.cs
+++ MyThreadPool/MyThreadPool/MyThreadPool.cs
@@ -1,13 +1,24 @@
+// <copyright file="MyThreadPool.cs" company="IlyaSotnikov">
+// Copyright (c) IlyaSotnikov. All rights reserved.
+// </copyright>
+
 namespace MyThreadPool;
 
+/// <summary>
+/// Represents a thread pool for executing tasksin multiple threads.
+/// </summary>
 public class MyThreadPool
 {
     private readonly Thread[] threads;
-    private readonly Queue<Action> tasksQueue = new();
-    private readonly CancellationTokenSource cancellationSource = new();
-    private readonly AutoResetEvent queueAccess = new(true);
-    private AutoResetEvent gettingStarted = new(false);
+    private readonly Queue<Action> tasksQueue = new ();
+    private readonly CancellationTokenSource cancellationSource = new ();
+    private readonly AutoResetEvent gettingStarted = new (false);
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MyThreadPool"/> class.
+    /// </summary>
+    /// <param name="numberOfThreads">The number of threads.</param>
+    /// <exception cref="ArgumentException">Throws if number of threads is not positive.</exception>
     public MyThreadPool(int numberOfThreads)
     {
         if (numberOfThreads <= 0)
@@ -15,42 +26,53 @@ public class MyThreadPool
             throw new ArgumentException("Number of threads must be positive", nameof(numberOfThreads));
         }
 
-        threads = new Thread[numberOfThreads];
+        this.threads = new Thread[numberOfThreads];
 
         for (int i = 0; i < numberOfThreads; ++i)
         {
-            threads[i] = new Thread(() => PerformTasks(cancellationSource.Token));
-            threads[i].Start();
+            this.threads[i] = new Thread(() => this.PerformTasks(this.cancellationSource.Token));
+            this.threads[i].Start();
         }
     }
 
+    /// <summary>
+    /// Adds a new task to the pool.
+    /// </summary>
+    /// <typeparam name="TResult">The type of function execution result.</typeparam>
+    /// <param name="function">Function to be performed in the task.</param>
+    /// <returns>Task object.</returns>
+    /// <exception cref="OperationCanceledException">Throws if threadpool was shutted down.</exception>
     public IMyTask<TResult> Submit<TResult>(Func<TResult> function)
     {
-        if (cancellationSource.IsCancellationRequested)
+        if (this.cancellationSource.IsCancellationRequested)
         {
-            throw new OperationCanceledException("The threadpool was shutted down.", cancellationSource.Token);
+            throw new OperationCanceledException("The threadpool was shutted down.", this.cancellationSource.Token);
         }
 
         var newTask = new MyTask<TResult>(function, this);
 
-        queueAccess.WaitOne();
-        tasksQueue.Enqueue(newTask.Start);
-        queueAccess.Set();
+        lock (this.tasksQueue)
+        {
+            this.tasksQueue.Enqueue(newTask.Start);
+        }
 
-        gettingStarted.Set();
+        this.gettingStarted.Set();
 
         return newTask;
     }
 
+    /// <summary>
+    /// Shut down the threadpool.
+    /// </summary>
     public void Shutdown()
     {
-        if (cancellationSource.IsCancellationRequested)
+        if (this.cancellationSource.IsCancellationRequested)
         {
             return;
         }
 
         this.cancellationSource.Cancel();
-        foreach (var thread in threads)
+        foreach (var thread in this.threads)
         {
             thread.Join();
         }
@@ -60,100 +82,113 @@ public class MyThreadPool
     {
         while (!cancellationToken.IsCancellationRequested)
         {
-            gettingStarted.WaitOne();
-            queueAccess.WaitOne();
-            if (tasksQueue.Count == 0)
-            {
-                queueAccess.Set();
-                continue;
-            }
+            this.gettingStarted.WaitOne();
+            Action? task = null;
 
-            Action task = tasksQueue.Dequeue();
-            queueAccess.Set();
+            lock (this.tasksQueue)
+            {
+                if (this.tasksQueue.Count == 0)
+                {
+                    Monitor.PulseAll(this.tasksQueue);
+                    continue;
+                }
+
+                task = this.tasksQueue.Dequeue();
+            }
 
             task();
 
-            gettingStarted.Set();
+            this.gettingStarted.Set();
         }
-
-        queueAccess.Set();
     }
 
     private void AddContinuingTask(Action taskStartAction)
     {
-        queueAccess.WaitOne();
-        tasksQueue.Enqueue(taskStartAction);
-        queueAccess.Set();
+        lock (this.tasksQueue)
+        {
+            this.tasksQueue.Enqueue(taskStartAction);
+        }
 
-        gettingStarted.Set();
+        this.gettingStarted.Set();
     }
 
+    /// <summary>
+    /// Represents a task that executes function.
+    /// </summary>
+    /// <typeparam name="TResult">The type of function result.</typeparam>
+    /// <param name="function">The function to be executed.</param>
+    /// <param name="threadPool">The trhread pool object.</param>
     public class MyTask<TResult>(Func<TResult> function, MyThreadPool threadPool) : IMyTask<TResult>
     {
         private readonly MyThreadPool threadPool = threadPool;
         private readonly Func<TResult> function = function;
+        private readonly ManualResetEvent resultReadiness = new (false);
         private TResult? result;
         private Exception? thrownException = null;
         private Action? continuingTaskStartAction = null;
-        private ManualResetEvent ResultReadiness = new (false);
 
+        /// <inheritdoc/>
         public bool IsCompleted { get; private set; } = false;
 
+        /// <inheritdoc/>
+        public TResult Result
+        {
+            get
+            {
+                this.resultReadiness.WaitOne();
+
+                if (this.thrownException != null)
+                {
+                    throw new AggregateException("Task failed.", this.thrownException);
+                }
+
+                if (this.result == null)
+                {
+                    throw new ArgumentException("Function does not return a result.");
+                }
+
+                return this.result;
+            }
+        }
+
+        /// <summary>
+        /// Starts the task.
+        /// </summary>
         public void Start()
         {
-            if (IsCompleted)
+            if (this.IsCompleted)
             {
                 return;
             }
 
             try
             {
-
-                result = function();
-                IsCompleted = true;
+                this.result = this.function();
+                this.IsCompleted = true;
             }
             catch (Exception ex)
             {
-                thrownException = ex;
+                this.thrownException = ex;
             }
 
-            ResultReadiness.Set();
+            this.resultReadiness.Set();
 
-            if (continuingTaskStartAction != null)
+            if (this.continuingTaskStartAction != null)
             {
-                threadPool.AddContinuingTask(continuingTaskStartAction!);
+                this.threadPool.AddContinuingTask(this.continuingTaskStartAction!);
             }
         }
 
-        public TResult Result
-        {
-            get
-            {
-                ResultReadiness.WaitOne();
-                
-                if (thrownException != null)
-                {
-                    throw new AggregateException("Task failed.", thrownException);
-                }
-                
-                if (result == null)
-                {
-                    throw new ArgumentException("Function does not return a result.");
-                }
-
-                return result;
-            }
-        }
-
+        /// <inheritdoc/>
         IMyTask<TNewResult> IMyTask<TResult>.ContinueWith<TNewResult>(Func<TResult, TNewResult> nextTask)
         {
-            if (IsCompleted)
+            if (this.IsCompleted)
             {
-                return threadPool.Submit(() => nextTask(Result));
+                return this.threadPool.Submit(() => nextTask(this.Result));
             }
 
-            var continuingTask = new MyTask<TNewResult>(() => nextTask(Result), threadPool);
-            continuingTaskStartAction = continuingTask.Start;
+            var continuingTask = new MyTask<TNewResult>(() => nextTask(this.Result), this.threadPool);
+            this.continuingTaskStartAction = continuingTask.Start;
 
             return continuingTask;
         }
--- MyThreadPool/MyThreadPoolTest/MyThreadPoolTests.cs
+++ MyThreadPool/MyThreadPoolTest/MyThreadPoolTests.cs
@@ -1,19 +1,33 @@
-namespace MyThreadPool2Tests;
+// <copyright file="MyThreadPoolTests.cs" company="IlyaSotnikov">
+// Copyright (c) IlyaSotnikov. All rights reserved.
+// </copyright>
+
+namespace MyThreadPoolTests;
 
 using NUnit.Framework;
 using MyThreadPool;
 
+/// <summary>
+/// Tests for <see cref="MyThreadPool"/>.
+/// </summary>
 public class Tests
 {
+    /// <summary>
+    /// Tests that number of active threads is equal to number of threads given to the constructor.
+    /// </summary>
     [Test]
     public static void Test_NumberOfActiveThreads_NumberGivenToConstructor()
     {
         var numberOfThreads = 6;
-        var threadPool = new MyThreadPool2(numberOfThreads);
+        var threadPool = new MyThreadPool(numberOfThreads);
         var tasks = new IMyTask<int>[numberOfThreads];
         for (int i = 0; i < numberOfThreads; ++i)
         {
-            tasks[i] = threadPool.Submit(() => { Thread.Sleep(100); return 1; });
+            tasks[i] = threadPool.Submit(() =>
+            {
+                Thread.Sleep(100);
+                return 1;
+            });
         }
 
         Thread.Sleep(110);
@@ -24,12 +38,15 @@ public class Tests
         }
     }
 
+    /// <summary>
+    /// Tests that works correctly when threads is more than tasks.
+    /// </summary>
     [Test]
     public static void Test_ThreadsMoreThenTasks_WorksCorrect()
     {
         var numberOfThreads = 20;
         var numberOfTasks = 10;
-        var threadPool = new MyThreadPool2(numberOfThreads);
+        var threadPool = new MyThreadPool(numberOfThreads);
         var tasks = new IMyTask<int>[numberOfTasks];
 
         for (int i = 0; i < numberOfTasks; ++i)
@@ -44,12 +61,15 @@ public class Tests
         }
     }
 
+    /// <summary>
+    /// Tests that works correctly when tasks is more than threads.
+    /// </summary>
     [Test]
     public static void Test_TasksMoreThenThreads_WorksCorrect()
     {
         var numberOfThreads = 10;
         var numberOfTasks = 20;
-        var threadPool = new MyThreadPool2(numberOfThreads);
+        var threadPool = new MyThreadPool(numberOfThreads);
         var tasks = new IMyTask<int>[numberOfTasks];
         for (int i = 0; i < numberOfTasks; ++i)
         {
@@ -65,34 +85,43 @@ public class Tests
         }
     }
 
+    /// <summary>
+    /// Tests that works correctly when tasks have different types.
+    /// </summary>
     [Test]
     public static void Test_DifferentTypeTasks_WorksCorrect()
     {
         var numberOfThreads = 4;
-        var threadPool = new MyThreadPool2(numberOfThreads);
-        var task1 = threadPool.Submit(() => 341 + 12 * 11);
+        var threadPool = new MyThreadPool(numberOfThreads);
+        var task1 = threadPool.Submit(() => 341 + (12 * 11));
         var task2 = threadPool.Submit(() => "Cucumber" + " banana");
         var task3 = threadPool.Submit(() => 7 % 2 == 0);
         var task4 = threadPool.Submit(() => "12345".ToArray());
 
-        AssertBlock(task1, 341 + 12 * 11);
+        AssertBlock(task1, 341 + (12 * 11));
         AssertBlock(task2, "Cucumber banana");
         AssertBlock(task3, false);
         AssertBlock(task4, ['1', '2', '3', '4', '5']);
     }
 
+    /// <summary>
+    /// Tests that throws exception when incorrect number of threads.
+    /// </summary>
     [Test]
     public static void Test_IncorrectNumberOfThreads_ThrowsException()
     {
-        Assert.Throws<ArgumentException>(() => new MyThreadPool2(-10));
-        Assert.Throws<ArgumentException>(() => new MyThreadPool2(0));
+        Assert.Throws<ArgumentException>(() => new MyThreadPool(-10));
+        Assert.Throws<ArgumentException>(() => new MyThreadPool(0));
     }
 
+    /// <summary>
+    /// Tests that task exceptions are passed up.
+    /// </summary>
     [Test]
     public static void Test_TaskThrowsException_PassedUp()
     {
         var numberOfThreads = 6;
-        var threadPool = new MyThreadPool2(numberOfThreads);
+        var threadPool = new MyThreadPool(numberOfThreads);
         var task = threadPool.Submit<int>(() => throw new ArgumentException());
         var exception = Assert.Throws<AggregateException>(() => { int a = task.Result; });
         Assert.Multiple(() =>
@@ -103,27 +132,36 @@ public class Tests
         });
     }
 
+    /// <summary>
+    /// Tests ContinueWith method.
+    /// </summary>
     [Test]
     public static void Test_ContinueWith_ReturnsCorrect()
     {
         var numberOfThreads = 2;
-        var threadPool = new MyThreadPool2(numberOfThreads);
+        var threadPool = new MyThreadPool(numberOfThreads);
         var task = threadPool.Submit(() => "12" + "34").ContinueWith((number) => int.Parse(number.ToArray()));
         AssertBlock(task, 1234);
     }
 
+    /// <summary>
+    /// Tests that many ContinueWith works correctly.
+    /// </summary>
     [Test]
     public static void Test_ManyContinueWith_ReturnsCorrect()
     {
-        var threadPool = new MyThreadPool2(2);
+        var threadPool = new MyThreadPool(2);
         var task = threadPool.Submit(() => "12" + "34").ContinueWith((number) => int.Parse(number.ToArray())).ContinueWith((number) => number * 2);
         AssertBlock(task, 2468);
     }
 
+    /// <summary>
+    /// Tests that after shutting down the tasks are executed.
+    /// </summary>
     [Test]
     public static void Test_Shutdown_AllTasksComplete()
     {
-        var threadPool = new MyThreadPool2(2);
+        var threadPool = new MyThreadPool(2);
         var flag = false;
 
         threadPool.Submit(() =>
