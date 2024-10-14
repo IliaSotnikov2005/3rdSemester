fd0d5878b82dd9baca3cd21d1762568012d5b2af
Ilya Sotnikov <ilia12st@yandex.ru>
2024-10-15 02:47:00 +0300

    start working

---
+++ SimpleFTP/SimpleFTP/Client.cs
@@ -0,0 +1,9 @@
+namespace SimpleFTP;
+
+public class Client(string host, int port)
+{
+    public string Host {get;} = host;
+    public int Port {get;} = port;
+
+    
+}
---
+++ SimpleFTP/SimpleFTP/Server.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+namespace SimpleFTP;
+
+public class Server(string localAddress, int port)
+{
+    private readonly TcpListener tcpListener = new (IPAddress.Parse(localAddress), port);
+    private CancellationTokenSource cancellationToken;
+
+    public void Start()
+    {
+        cancellationToken = new CancellationTokenSource();
+        tcpListener.Start();
+        _ = Listen();
+    }
+
+    public void Stop()
+    {
+        tcpListener.Stop();
+    }
+
+    private async Task HandleConnection(Socket clientConnection)
+    {
+        return;
+    }
+
+    private async Task Listen()
+    {
+        List<Task> clientConnectionsTasks = [];
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            Socket clientConnection = await tcpListener.AcceptSocketAsync(cancellationToken.Token);
+            clientConnectionsTasks.Add(HandleConnection(clientConnection));
+        }
+
+        foreach (var clientConnectionTask in clientConnectionsTasks)
+        {
+            clientConnectionTask.Wait();
+        }
+    }
+}
