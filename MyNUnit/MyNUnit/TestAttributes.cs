c02ea7d0538049a6b0f3264c44984e7fe2389ec7
Ilya Sotnikov <ilia12st@yandex.ru>
2024-11-07 23:42:49 +0300

    fix style

--- MyNUnit/MyNUnit/TestAttributes.cs
+++ MyNUnit/MyNUnit/TestAttributes.cs
@@ -1,52 +1,78 @@
-using System.Reflection;
+// <copyright file="TestAttributes.cs" company="IlyaSotnikov">
+// Copyright (c) IlyaSotnikov. All rights reserved.
+// </copyright>
 
 namespace MyNUnit;
 
+/// <summary>
+/// Attribute for my tests.
+/// </summary>
 [AttributeUsage(AttributeTargets.Method)]
 public class MyTestAttribute : Attribute
 {
-    public Type? Expected { get; }
-    public string? Ignore { get; }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MyTestAttribute"/> class.
+    /// </summary>
+    public MyTestAttribute()
+    {
+    }
 
-    public MyTestAttribute() { }
-    public MyTestAttribute(Type expected) => Expected = expected;
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MyTestAttribute"/> class.
+    /// </summary>
+    /// <param name="expected">Expected exception.</param>
+    public MyTestAttribute(Type expected) => this.Expected = expected;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MyTestAttribute"/> class.
+    /// </summary>
+    /// <param name="expected">Expected exception.</param>
+    /// <param name="ignore">Comment why ignore the test.</param>
     public MyTestAttribute(Type expected, string ignore)
     {
-        Expected = expected;
-        Ignore = ignore;
+        this.Expected = expected;
+        this.Ignore = ignore;
     }
+
+    /// <summary>
+    /// Gets expected exception.
+    /// </summary>
+    public Type? Expected { get; }
+
+    /// <summary>
+    /// Gets comment why ignore the test.
+    /// </summary>
+    public string? Ignore { get; }
 }
 
+/// <summary>
+/// Attribute for method that runs before the test.
+/// </summary>
 [AttributeUsage(AttributeTargets.Method)]
 public class BeforeAttribute : Attribute
 {
 }
 
+/// <summary>
+/// Attribute for method that runs after the test.
+/// </summary>
 [AttributeUsage(AttributeTargets.Method)]
 public class AfterAttribute : Attribute
 {
 }
 
+/// <summary>
+/// Attribute for method that runs before the test class.
+/// </summary>
 [AttributeUsage(AttributeTargets.Method)]
 public class BeforeClassAttribute : Attribute
 {
-    // public BeforeClassAttribute(MethodInfo method)
-    // {
-    //     if (!method.IsStatic)
-    //     {
-    //         throw new InvalidOperationException("The method must be static.");
-    //     }
-    // }
 }
 
+/// <summary>
+/// Attribute for method that runs after the test class.
+/// </summary>
 [AttributeUsage(AttributeTargets.Method)]
 public class AfterClassAttribute : Attribute
 {
-    // public AfterClassAttribute(MethodInfo method)
-    // {
-    //     if (!method.IsStatic)
-    //     {
-    //         throw new InvalidOperationException("The method must be static.");
-    //     }
-    // }
 }
