3465322593c3bc5039a92a86e691f951296d64a6
Ilya Sotnikov <ilia12st@yandex.ru>
2024-12-29 20:50:00 +0300

    redo my thread pool

---
+++ MyThreadPool/MyThreadPoolTest/MyThreadPoolTests.cs
@@ -0,0 +1,315 @@
+﻿// <copyright file="MyThreadPoolTests.cs" company="IlyaSotnikov">
+// Copyright (c) IlyaSotnikov. All rights reserved.
+// </copyright>
+
+namespace MyThreadPoolTests;
+
+using MyThreadPoolSpace;
+
+/// <summary>
+/// Tests for <see cref="MyThreadPool"/>.
+/// </summary>
+public class MyThreadPoolTests
+{
+    /// <summary>
+    /// Tests that number of active threads is equal to number of threads given to the constructor.
+    /// </summary>
+    [Test]
+    public static void Test_NumberOfActiveThreads_NumberGivenToConstructor()
+    {
+        var numberOfThreads = 6;
+        var threadPool = new MyThreadPool(numberOfThreads);
+        var tasks = new IMyTask<int>[numberOfThreads];
+        for (int i = 0; i < numberOfThreads; ++i)
+        {
+            tasks[i] = threadPool.Submit(() =>
+            {
+                Thread.Sleep(100);
+                return 1;
+            });
+        }
+
+        Thread.Sleep(110);
+
+        foreach (var task in tasks)
+        {
+            AssertBlock(task, 1);
+        }
+
+        threadPool.Shutdown();
+    }
+
+    /// <summary>
+    /// Tests that works correctly when threads is more than tasks.
+    /// </summary>
+    [Test]
+    public static void Test_ThreadsMoreThanTasks_WorksCorrect()
+    {
+        var numberOfThreads = 20;
+        var numberOfTasks = 10;
+        var threadPool = new MyThreadPool(numberOfThreads);
+        var tasks = new IMyTask<int>[numberOfTasks];
+
+        for (int i = 0; i < numberOfTasks; ++i)
+        {
+            var localI = i;
+            tasks[i] = threadPool.Submit(() => localI * 2);
+        }
+
+        for (int i = 0; i < numberOfTasks; ++i)
+        {
+            AssertBlock(tasks[i], i * 2);
+        }
+
+        threadPool.Shutdown();
+    }
+
+    /// <summary>
+    /// Tests that works correctly when tasks is more than threads.
+    /// </summary>
+    [Test]
+    public static void Test_TasksMoreThanThreads_WorksCorrect()
+    {
+        var numberOfThreads = 10;
+        var numberOfTasks = 20;
+        var threadPool = new MyThreadPool(numberOfThreads);
+        var tasks = new IMyTask<int>[numberOfTasks];
+        for (int i = 0; i < numberOfTasks; ++i)
+        {
+            var localI = i;
+            tasks[i] = threadPool.Submit(() => localI * localI);
+        }
+
+        Thread.Sleep(200);
+
+        for (int i = 0; i < numberOfTasks; ++i)
+        {
+            AssertBlock(tasks[i], i * i);
+        }
+
+        threadPool.Shutdown();
+    }
+
+    /// <summary>
+    /// Tests that works correctly when tasks have different types.
+    /// </summary>
+    [Test]
+    public static void Test_DifferentTypeTasks_WorksCorrect()
+    {
+        var numberOfThreads = 4;
+        var threadPool = new MyThreadPool(numberOfThreads);
+        var task1 = threadPool.Submit(() => 341 + (12 * 11));
+        var task2 = threadPool.Submit(() => "Cucumber" + " banana");
+        var task3 = threadPool.Submit(() => 7 % 2 == 0);
+        var task4 = threadPool.Submit(() => "12345".ToArray());
+
+        AssertBlock(task1, 341 + (12 * 11));
+        AssertBlock(task2, "Cucumber banana");
+        AssertBlock(task3, false);
+        AssertBlock(task4, ['1', '2', '3', '4', '5']);
+
+        threadPool.Shutdown();
+    }
+
+    /// <summary>
+    /// Tests that throws exception when incorrect number of threads.
+    /// </summary>
+    [Test]
+    public static void Test_IncorrectNumberOfThreads_ThrowsException()
+    {
+        Assert.Throws<ArgumentException>(() => new MyThreadPool(-10));
+        Assert.Throws<ArgumentException>(() => new MyThreadPool(0));
+    }
+
+    /// <summary>
+    /// Tests that task exceptions are passed up.
+    /// </summary>
+    [Test]
+    public static void Test_TaskThrowsException_PassedUp()
+    {
+        var numberOfThreads = 6;
+        var threadPool = new MyThreadPool(numberOfThreads);
+        var task = threadPool.Submit<int>(() => throw new ArgumentException());
+        var exception = Assert.Throws<AggregateException>(() => { int a = task.Result; });
+        Assert.That(exception, Is.Not.Null);
+        if (exception is not null)
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(exception.InnerException, Is.Not.Null);
+                Assert.That(exception.InnerException!.GetType(), Is.EqualTo(typeof(ArgumentException)));
+            });
+        }
+
+        threadPool.Shutdown();
+    }
+
+    /// <summary>
+    /// Tests ContinueWith method.
+    /// </summary>
+    [Test]
+    public static void Test_ContinueWith_ReturnsCorrect()
+    {
+        var numberOfThreads = 2;
+        var threadPool = new MyThreadPool(numberOfThreads);
+        var task = threadPool.Submit(() => "12" + "34").ContinueWith((number) => int.Parse(number));
+        AssertBlock(task, 1234);
+        threadPool.Shutdown();
+    }
+
+    /// <summary>
+    /// Tests that many ContinueWith works correctly.
+    /// </summary>
+    [Test]
+    public static void Test_ManyContinueWith_ReturnsCorrect()
+    {
+        var threadPool = new MyThreadPool(2);
+        var task = threadPool.Submit(() => "12" + "34").ContinueWith((number) => int.Parse(number.ToArray())).ContinueWith((number) => number * 2);
+        AssertBlock(task, 2468);
+
+        threadPool.Shutdown();
+    }
+
+    /// <summary>
+    /// Tests that after shutting down the tasks are executed.
+    /// </summary>
+    [Test]
+    public static void Test_Shutdown_AllTasksComplete()
+    {
+        var threadPool = new MyThreadPool(2);
+        var flag = false;
+
+        threadPool.Submit(() =>
+        {
+            Thread.Sleep(500);
+            Volatile.Write(ref flag, true);
+            return 0;
+        });
+
+        threadPool.Shutdown();
+
+        Assert.That(flag, Is.True);
+    }
+
+    /// <summary>
+    /// Tests that thread pool thread safety with Submit and Shutdown.
+    /// </summary>
+    [Test]
+    public static void Test_ThreadSafety_MethodsCalledFromMultipleThreads()
+    {
+        var numberOfThreads = 4;
+        var threadPool = new MyThreadPool(numberOfThreads);
+        var tasksCount = 10;
+        var exceptions = new List<Exception>();
+
+        var tasks = new List<IMyTask<string>>();
+
+        var submitThread = new Thread(() =>
+        {
+            for (int i = 0; i < tasksCount; i++)
+            {
+                try
+                {
+                    var task = threadPool.Submit(() =>
+                    {
+                        Thread.Sleep(100);
+                        return $"Task {i} completed";
+                    });
+                    tasks.Add(task);
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+        });
+
+        var shutdownThread = new Thread(() =>
+        {
+            Thread.Sleep(100);
+            try
+            {
+                threadPool.Shutdown();
+            }
+            catch (Exception ex)
+            {
+                exceptions.Add(ex);
+            }
+        });
+
+        submitThread.Start();
+        shutdownThread.Start();
+
+        submitThread.Join();
+        shutdownThread.Join();
+
+        Assert.That(exceptions, Is.Empty);
+
+        foreach (var task in tasks)
+        {
+            Assert.That(task.IsCompleted, Is.True);
+        }
+    }
+
+    /// <summary>
+    /// Tests that thread pool thread safety with ContinueWith and Shutdown.
+    /// </summary>
+    [Test]
+    public static void Test_ThreadSafety_ContinueWithAndShutdown()
+    {
+        var numberOfThreads = 4;
+        var threadPool = new MyThreadPool(numberOfThreads);
+        var tasksCount = 10;
+        var exceptions = new List<Exception>();
+
+        var tasks = new List<IMyTask<int>>();
+
+        var continueWithThread = new Thread(() =>
+        {
+            for (int i = 0; i < tasksCount; i++)
+            {
+                try
+                {
+                    var locali = i;
+                    var task = threadPool.Submit(() => locali);
+                    var continuedTask = task.ContinueWith((result) =>
+                    {
+                        Thread.Sleep(100);
+                        return result + 1;
+                    });
+
+                    tasks.Add(continuedTask);
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+        });
+
+        var shutdownThread = new Thread(() =>
+        {
+            Thread.Sleep(200);
+            threadPool.Shutdown();
+        });
+
+        continueWithThread.Start();
+        shutdownThread.Start();
+
+        continueWithThread.Join();
+        shutdownThread.Join();
+
+        Assert.That(exceptions, Is.Empty);
+
+        foreach (var task in tasks)
+        {
+            Assert.That(task.IsCompleted, Is.True);
+        }
+    }
+
+    private static void AssertBlock<T>(IMyTask<T> task, T expected)
+    {
+        Assert.That(task.Result, Is.EqualTo(expected));
+        Assert.That(task.IsCompleted, Is.True);
+    }
+}
