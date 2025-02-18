b33ff22e752acbe293b06ecc16df0e1cb6fbf7f9
ilia Sotnikov <ilia12st@yandex.ru>
2025-02-18 14:30:39 +0300

    small fix

--- MyNUnit/MyNUnitTests/MyNUnitTests.cs
+++ MyNUnit/MyNUnitTests/MyNUnitTests.cs
@@ -16,37 +16,40 @@ public class NUnitTests
     /// <summary>
     /// Checks that throws exception if path not exists.
     /// </summary>
-    //[Test]
-    //public static void Test_ThrowsDirectoryNotFoundException()
-    //{
-    //    Assert.ThrowsAsync<DirectoryNotFoundException>(async () => await MyTester.RunTestsFromDirectory("bambam/net8.0"));
-    //}
+    [Test]
+    public static void Test_ThrowsDirectoryNotFoundException()
+    {
+        Assert.ThrowsAsync<DirectoryNotFoundException>(async () => await MyTester.RunTestsFromDirectory("bambam/net8.0"));
+    }
 
     /// <summary>
     /// Checks that tests runs correctly.
     /// </summary>
     /// <returns>Task.</returns>
-    //[Test]
-    //public async Task Test_RunsTestsCorrectly()
-    //{
-    //    var result = await MyTester.RunTestsFromDirectory("../../../../ProjectForTesting/bin/Debug/net9.0");
+    [Test]
+    public async Task Test_RunsTestsCorrectly()
+    {
+        var result = await MyTester.RunTestsFromDirectory("../../../../ProjectForTesting/bin/Debug/net9.0");
 
-    //    var expected = new List<TestResult>
-    //    {
-    //    new("Test_ShouldBePassed", TestStatus.Passed, string.Empty),
-    //    new("Test_ShouldBeIgnored", TestStatus.Ignored, "ignore"),
-    //    new("Test_ShouldBeFailed", TestStatus.Failed, "Exception has been thrown by the target of an invocation."),
-    //    new("Test_ShouldThrowException", TestStatus.Passed, "Throwed expected exception."),
-    //    };
+        var expected = new List<TestResult>
+        {
+        new("Test_ShouldBePassed", TestStatus.Passed, string.Empty),
+        new("Test_ShouldBeIgnored", TestStatus.Ignored, "ignore"),
+        new("Test_ShouldBeFailed", TestStatus.Failed, "Exception has been thrown by the target of an invocation."),
+        new("Test_ShouldThrowException", TestStatus.Passed, "Throwed expected exception."),
+        };
 
-    //    foreach (var testClass in result)
-    //    {
-    //        for (int i = 0; i < testClass.TestResults.Count; ++i)
-    //        {
-    //            Assert.That(this.Comparer(testClass.TestResults[i], expected[i]), Is.True);
-    //        }
-    //    }
-    //}
+        foreach (var testAssembly in result.TestAssemblyResults)
+        {
+            foreach (var testClass in testAssembly.TestClassResults)
+            {
+                for (int i = 0; i < testClass.TestResults.Count; ++i)
+                {
+                    Assert.That(this.Comparer(testClass.TestResults[i], expected[i]), Is.True);
+                }
+            }
+        }
+    }
 
     private bool Comparer(TestResult actual, TestResult expected)
     {
